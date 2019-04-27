ccd4ef9ee66dbfd5d52ac84d1e8c3ea87c008160
Hekar Khani <hekark@gmail.com>
2019-04-27 17:38:33 -0400

    Fixed padding on entities page
    - leading-normal for line spacing
    - padding on left and right for N/A

--- src/pages/entities.js
+++ src/pages/entities.js
@@ -44,7 +44,7 @@ function renderFgd(content) {
           } else {
             return {
               key,
-              val: String(val)
+              val: <p className="py-2 px-4">String(val)</p>
             };
           }
         })
@@ -63,7 +63,7 @@ function renderFgd(content) {
         </table>
       );
     } else {
-      return 'N/A';
+      return <p className="py-2 px-4">N/A</p>;
     }
   };
 
@@ -76,9 +76,9 @@ function renderFgd(content) {
             href={'#' + entity.name}>{entity.name}</a>
         </h3>
         <p className="mt-4">Type: <span className="font-bold">{entity.type ? entity.type : '...'}</span></p>
-        <h5 className="mt-4 leading-normal">Description</h5>
+        <h5 className="mt-4">Description</h5>
         <p>{entity.description ? entity.description : 'none'}</p>
-        <h5 className="mt-4 leading-normal">Properties</h5>
+        <h5 className="mt-4">Properties</h5>
         {renderJson(entity.properties)}
         <h5 className="mt-4">Inputs</h5>
         {renderJson(entity.inputs)}
@@ -100,45 +100,47 @@ function EntitiesPage() {
         title="Entities"
         keywords={[`luminousforts`, `sourcesdk`, `sourceforts`, `halflife2`, 'entities']}
       />
-      <h1 className="">Entities</h1>
-      <p className="mt-4">The LuminousForts entities visible in Hammer</p>
-      <p className="mt-4"><a href="https://developer.valvesoftware.com/wiki/FGD">FGD</a> files are found in the <a href="https://github.com/hekar/luminousforts/tree/master/fgd">repository fgd folder</a></p>
+      <div className="container leading-normal">
+        <h1 className="">Entities</h1>
+        <p className="mt-4">The LuminousForts entities visible in Hammer</p>
+        <p className="mt-4"><a href="https://developer.valvesoftware.com/wiki/FGD">FGD</a> files are found in the <a href="https://github.com/hekar/luminousforts/tree/master/fgd">repository fgd folder</a></p>
 
-      <h2 className="mt-16">Shared</h2>
-      <p className="mt-4">
-        Entities available in any gamemode
-      </p>
-      <div className="mt-4 bg-blue-lightest border-t border-b border-blue text-blue-dark px-4 py-3" role="alert">
-        <p className="text-sm">All these entities are prefixed with lf_* and apply to multiple gamemodes</p>
-      </div>
-      {renderFgd(fgd.shared)}
+        <h2 className="mt-16">Shared</h2>
+        <p className="mt-4">
+          Entities available in any gamemode
+        </p>
+        <div className="mt-4 bg-blue-lightest border-t border-b border-blue text-blue-dark px-4 py-3" role="alert">
+          <p className="text-sm">All these entities are prefixed with lf_* and apply to multiple gamemodes</p>
+        </div>
+        {renderFgd(fgd.shared)}
 
-      <h2 className="mt-16">Classic Gamemode</h2>
-      <p className="mt-4">
-        Entities only available in the Classic (Capture the flag) gamemode
-      </p>
-      <div className="mt-4 bg-blue-lightest border-t border-b border-blue text-blue-dark px-4 py-3" role="alert">
-        <p className="text-sm">All entities are prefixed with lfc_*</p>
-      </div>
-      {renderFgd(fgd.classic)}
+        <h2 className="mt-16">Classic Gamemode</h2>
+        <p className="mt-4">
+          Entities only available in the Classic (Capture the flag) gamemode
+        </p>
+        <div className="mt-4 bg-blue-lightest border-t border-b border-blue text-blue-dark px-4 py-3" role="alert">
+          <p className="text-sm">All entities are prefixed with lfc_*</p>
+        </div>
+        {renderFgd(fgd.classic)}
 
-      <h2 className="mt-16">Domination Gamemode</h2>
-      <p className="mt-4">
-        Entities only available in the Domination gamemode
-      </p>
-      <div className="mt-4 bg-blue-lightest border-t border-b border-blue text-blue-dark px-4 py-3" role="alert">
-        <p className="text-sm">All entities are prefixed with lfd_*</p>
-      </div>
-      {renderFgd(fgd.domination)}
+        <h2 className="mt-16">Domination Gamemode</h2>
+        <p className="mt-4">
+          Entities only available in the Domination gamemode
+        </p>
+        <div className="mt-4 bg-blue-lightest border-t border-b border-blue text-blue-dark px-4 py-3" role="alert">
+          <p className="text-sm">All entities are prefixed with lfd_*</p>
+        </div>
+        {renderFgd(fgd.domination)}
 
-      <h2 className="mt-16">Custom Gamemode</h2>
-      <p className="mt-4">
-        Available for custom gamemodes
-      </p>
-      <div className="mt-4 bg-blue-lightest border-t border-b border-blue text-blue-dark px-4 py-3" role="alert">
-        <p className="text-sm">All these entities are for custom gameplay</p>
+        <h2 className="mt-16">Custom Gamemode</h2>
+        <p className="mt-4">
+          Available for custom gamemodes
+        </p>
+        <div className="mt-4 bg-blue-lightest border-t border-b border-blue text-blue-dark px-4 py-3" role="alert">
+          <p className="text-sm">All these entities are for custom gameplay</p>
+        </div>
+        {renderFgd(fgd.custom)}
       </div>
-      {renderFgd(fgd.custom)}
     </Layout>
   );
 }
